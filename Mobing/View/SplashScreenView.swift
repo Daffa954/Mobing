266b9985dacfdb2cedf9c5344baafb2d2799414d
souiyaaa <153163259+souiyaaa@users.noreply.github.com>
2025-06-19 19:26:06 +0700

    Updated splash scree

--- Mobing/View/SplashScreenView.swift
+++ Mobing/View/SplashScreenView.swift
@@ -6,40 +6,52 @@
 //
 
 import Foundation
-
-
 import SwiftUI
 
 struct SplashScreenView: View {
     @State private var isActive = false
-    
+
     var body: some View {
         if isActive {
-          MainView()
+            MainView()
         } else {
             VStack {
-                Spacer()
-                Image("logo")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    
-                Text("MOBING")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, -20)
-                    .foregroundColor(.white)
+                HStack {
+                    Text("The future of \n second hand \n electric car")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Spacer()
+                }.padding(.leading, 12)
+                    .padding(.top, 90)
+
+                //show me the image of a car from my asset called "tesla mobil "
+
+                GeometryReader { geometry in
+                    Image("tesla mobil")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(
+                            width: geometry.size.width,
+                        )
+                        .offset(x: -200, y:100)
+                        .clipped()
+                        
+                }
+                .frame(height: 500)
                 
+
                 Spacer()
                 Text("By UD Maju Sentosa")
                     .font(.callout)
                     .fontWeight(.medium)
                     .padding(.bottom, 50)
                     .foregroundColor(.white)
-                
+
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color("skyBlue"))
-                .ignoresSafeArea()
+            .background(Color.black)
+            .ignoresSafeArea()
             .onAppear {
                 // Waktu tampil splash screen dalam detik
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
